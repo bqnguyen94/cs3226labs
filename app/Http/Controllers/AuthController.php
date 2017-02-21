07a7c04c7b252faa54a7b762de34d6f25d9e4f7d
Julian Chan <frozzenhart@hotmail.com>
2017-02-21 17:56:33 +0800

    Registers users (validates adding of user and creates new user)

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -34,6 +34,17 @@ class AuthController extends Controller {
         }
     }
 
+    public function register(Request $request)
+    {
+        $user = new User($request->only(['name', 'email']));
+        $user->password = bcrypt($request->password);
+        $user->role = User::ROLE_USER;
+        $user->save();
+
+        Session::flash('alert-success', "Added Successfully");
+        return back();
+    }
+
     public function logout() {
         if (Auth::check()) {
             Auth::logout();
