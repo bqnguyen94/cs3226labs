561396f0a6c2be7247621d9ac9dba0a892009a56
Nguyen Quoc Bao <ryuu.sakamoto.164@outlook.com>
2017-02-24 02:46:55 +0800

    Add student and score seeder and pictures

---
+++ database/seeds/StudentsAndScoresSeeder.php
@@ -0,0 +1,107 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Student;
+use App\Score;
+
+class StudentsAndScoresSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = Faker\Factory::create();
+
+        $limit = 50;
+
+        $iso3_codes = json_decode(file_get_contents('iso3.json'), true);
+
+        for ($i = 1; $i <= $limit; $i++) {
+            $firstName = $faker->firstName;
+            $lastName = $faker->lastName;
+            $name = $firstName . " " . $lastName;
+            $kattis = strtolower($firstName);
+            $image = "/img/icons/" . (string) ($i) . ".png";
+            $country_iso2 = $faker->countryCode;
+            $country_iso3 = $iso3_codes[$country_iso2];
+
+            $mc = "";
+            for ($j = 0; $j < 9; $j++) {
+                if ($j < 3) {
+                    $mc .= number_format($faker->numberBetween($min = 0, $max = 8) * 0.5, 1) . ",";
+                } else {
+                    $mc .= 'x.y,';
+                }
+            }
+            $mc = rtrim($mc, ",");
+
+            $tc = "";
+            for ($j = 0; $j < 2; $j++) {
+                $tc .= number_format($faker->numberBetween($min = 0, $max = 8) * 0.5, 1) . ",";
+            }
+            $tc = rtrim($tc, ",");
+
+            $hw = "";
+            for ($j = 0; $j < 10; $j++) {
+                if ($j < 2) {
+                    $hw .= number_format($faker->numberBetween($min = 0, $max = 4) * 0.5, 1) . ",";
+                } else {
+                    $hw .= 'x.y,';
+                }
+            }
+            $hw = rtrim($hw, ",");
+
+            $pb = "";
+            for ($j = 0; $j < 9; $j++) {
+                if ($j < 3) {
+                    $pb .= $faker->numberBetween($min = 0, $max = 1) . ",";
+                } else {
+                    $hw .= 'x,';
+                }
+            }
+            $pb = rtrim($pb, ",");
+
+            $ks = "";
+            for ($j = 0; $j < 12; $j++) {
+                if ($j < 4) {
+                    $ks = $faker->numberBetween($min = 0, $max = 1) . ",";
+                } else {
+                    $ks .= 'x,';
+                }
+            }
+            $ks = rtrim($ks, ",");
+
+            $ac = "";
+            for ($j = 0; $j < 3; $j++) {
+                if ($j < 3) {
+                    $ac .= $faker->numberBetween($min = 0, $max = 3) . ",";
+                } else {
+                    $ks .= 'x,';
+                }
+            }
+            $ac = rtrim($ac, ",");
+
+            $student = Student::create([
+                'nick' => $kattis,
+                'name' => $name,
+                'kattis' => $kattis,
+                'image' => $image,
+                'country_iso2' => $country_iso2,
+                'country_iso3' => $country_iso3,
+            ]);
+
+            Score::create([
+                'student_id' => $student->id,
+                'mc' => $mc,
+                'tc' => $tc,
+                'hw' => $hw,
+                'pb' => $pb,
+                'ks' => $ks,
+                'ac' => $ac,
+            ]);
+        }
+    }
+}
