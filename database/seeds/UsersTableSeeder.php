caa4bbbd1a2059384b69bead146d269705e42745
Julian Chan <frozzenhart@hotmail.com>
2017-02-24 00:20:15 +0800

    Added 3 seeds for the user table

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\User;
 use Illuminate\Database\Seeder;
 
 class UsersTableSeeder extends Seeder
@@ -12,5 +13,16 @@ class UsersTableSeeder extends Seeder
     public function run()
     {
         //
+        User::create([
+            'username'=>'admin','role'=>User::ROLE_ADMIN,'password'=>bcrypt('123456'),'email' => 'admin@unicorn.com'
+        ]);
+
+        User::create([
+            'username'=>'mod','role'=>User::ROLE_MODERATOR,'password'=>bcrypt('123456'),'email' => 'mod@unicorn.com'
+        ]);
+
+        User::create([
+            'username'=>'user','role'=>User::ROLE_USER,'password'=>bcrypt('123456'),'email' => 'user@unicorn.com'
+        ]);
     }
 }
