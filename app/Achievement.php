3d7f411502dcd37348f6e96646c126e76265b1f5
Julian Chan <frozzenhart@hotmail.com>
2017-02-25 15:02:46 +0800

    add in fillables for achievement model

--- app/Achievement.php
+++ app/Achievement.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Achievement extends Model
 {
     //
+    protected $fillable = ['achievement_name','stars'];
+
     public function students(){
         $this->belongsToMany('App\Student');
     }
