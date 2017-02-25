4ecce0c3499a94a65c7f40accb2c8012c6b06775
bubsnow <bubsnow@gmail.com>
2017-02-25 15:27:16 +0800

    Button with toggle :D

--- resources/views/change.blade.php
+++ resources/views/change.blade.php
@@ -1,20 +1,48 @@
 @extends('layouts.template') <!-- use template from previous slide -->
 @section('main') <!-- define a section called main -->
+
 <div class="container-fluid">
 	<table>
+		<script>
+			var x = 0;
+		</script>
 		<thead>
 			<th>Name</th>
 			<th>Change</th>
-			<th>Name</th>
-			<th>Change</th>
+			<th>Button to switch role</th>
+			<th>Button to update</th>
 		</thead>
 		<tbody>
 		
 			<tr>
 				<td>User</td>
 				<td>Role</td>
-				<td>Button to switch</td>
-				<td>Button to update</td>
+				<td>Retrieve current role
+				<div>
+					<p id="demo" onclick="myFunction()">Click me to change my text color.</p>
+
+					<script>
+						
+						var x = 1;
+					function myFunction() {
+						x = (1 + x)%2;
+						if(x == 1){
+							document.getElementById("demo").style.color = "red";
+						}else{
+							document.getElementById("demo").style.color = "blue";
+						}
+					}
+					</script>
+				</div>
+				
+				</td>
+				<td>Update changes to database
+				
+				<div>
+						
+				</div>
+				
+				</td>
 			</tr>
 			
 		</tbody>
@@ -23,5 +51,6 @@
 @stop
 
 @section('script')
-<script type="text/javascript" src="/js/index.js"></script>
+<script type="text/javascript" src="/js/index.js">
+</script>
 @stop
