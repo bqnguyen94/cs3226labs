eef6ed7389add56e27860d51cc202bda16dc6050
Juliancsw <frozzenhart@hotmail.com>
2017-02-28 23:03:26 +0800

    Added messages for Student model

--- app/Student.php
+++ app/Student.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Student extends Model
 {
-    protected $fillable = ['name', 'nick', 'image', 'kattis', 'country_iso2', 'country_iso3'];
+    protected $fillable = ['name','messageTo','messageFrom', 'nick', 'image', 'kattis', 'country_iso2', 'country_iso3'];
 
     public function score() {
         return $this.hasOne('Score');
