abddebd899ed0233ee756bc755176d094cf582bf
YIM CHIA HUI <E0003264@u.nus.edu>
2017-03-04 00:50:43 +0800

    Make Facebook link from online rather than file directory

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -47,7 +47,7 @@
   var js, fjs = d.getElementsByTagName(s)[0];
   if (d.getElementById(id)) return;
   js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.8";
+  js.src = "http://connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.8";
   fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));</script>
 
