4d4ad2fa33197335a1db4be04090af827cfe68c6
Nguyen Quoc Bao <ryuu.sakamoto.164@outlook.com>
2017-03-03 18:44:13 +0800

    Remove analytics from some pages

--- resources/views/chart.blade.php
+++ resources/views/chart.blade.php
@@ -12,4 +12,14 @@
     var json = <?php echo json_encode($data); ?>;
 </script>
 <script type="text/javascript" src="/js/multiseries-chart.js"></script>
+<script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-92961727-1', 'auto');
+    ga('send', 'pageview');
+
+</script>
 @stop
