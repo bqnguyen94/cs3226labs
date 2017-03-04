44a525e3be1bd0c251215fc7ca28d1cfecde3b30
Pure_Sero <pure943597795@gmail.com>
2017-03-05 02:41:25 +0800

    Chinese version

--- resources/lang/zh/lang.php
+++ resources/lang/zh/lang.php
@@ -1,5 +1,6 @@
 <?php
    return [
+   	  //Header
       'Rank List' => '列表',
       'Chart' => '曲线图',
       'Create' => '新建',
@@ -9,16 +10,38 @@
       'Help' => '帮助',
       'Register' => '注册',
       'Login' => '登录',
+      'Remember me' => '记住我',
+      'Sign in' => '登录',
+      'Forgot Your Password?' => '忘记密码？',
+      'Hi,' => '你好，',
+      'Messages' => '消息 ',
+      'Logout' => '注销',
+
+      //Index
       'R' => '序',
       'Flag' => '国籍',
-      'Student Name' => '姓名',
-      'MC' => 'MC',
-      'TC' => 'TC',
-      'SPE' => 'SPE',
-      'HW' => 'HW',
-      'Bs' => 'Bs',
-      'KS' => 'KS',
-      'Ac' => 'Ac',
-      'DIL' => 'DIL',
+      'Student Name' => '学生姓名',
       'Sum' => '总成绩',
+
+      //Create
+      'createTitle'=>'在CS3233课程2016/17学年第2学期中创建新的学生档案',
+      'Nick name:' => '昵称:',
+      'Full name:' => '全名:',
+      'Kattis account:' => 'Kattis 账户:',
+      'Nationality:' => '国籍:',
+      'Upload a profile picture' => '上传一张档案照',
+      'Submit' => '提交',
+
+      //batch
+      'Category:' => '类别:',
+      'Week:' => '星期:',
+      'Student' => '学生',
+      'Score' => '分数',
+      'Add Score' => '加分',
+
+      //update
+      'Updating in progress...' => ' 正在更新...',
+      'Select user' => '选择用户',
+      'Update email address' => '更新电子邮箱',
+      'To change current user type' => '更改用户权限',
    ];
