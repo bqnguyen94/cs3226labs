e67edcccb6f6e5e46754489e26479fd382a5af1c
Pure_Sero <pure943597795@gmail.com>
2017-03-05 02:32:52 +0800

    translation map

--- resources/lang/en/lang.php
+++ resources/lang/en/lang.php
@@ -1,5 +1,6 @@
 <?php
    return [
+   	  //Header
       'Rank List' => 'Rank List',
       'Chart' => 'Chart',
       'Create' => 'Create',
@@ -9,16 +10,38 @@
       'Help' => 'Help',
       'Register' => 'Register',
       'Login' => 'Login',
+      'Remember me' => 'Remember me',
+      'Sign in' => 'Sign in',
+      'Forgot Your Password?' => 'Forgot Your Password?',
+      'Hi,' => 'Hi, ',
+      'Messages' => 'Messages ',
+      'Logout' => 'Logout',
+
+      //Index
       'R' => 'R',
       'Flag' => 'Flag',
       'Student Name' => 'Student Name',
-      'MC' => 'MC',
-      'TC' => 'TC',
-      'SPE' => 'SPE',
-      'HW' => 'HW',
-      'Bs' => 'Bs',
-      'KS' => 'KS',
-      'Ac' => 'Ac',
-      'DIL' => 'DIL',
       'Sum' => 'Sum',
+
+      //Create
+      'createTitle'=>'Create New Student in CS3233 S2 AY 2016/17',
+      'Nick name:' => 'Nick name:',
+      'Full name:' => 'Full name:',
+      'Kattis account:' => 'Kattis account:',
+      'Nationality:' => 'Nationality:',
+      'Upload a profile picture' => 'Upload a profile picture',
+      'Submit' => 'Submit',
+
+      //batch
+      'Category:' => 'Category:',
+      'Week:' => 'Week:',
+      'Student' => 'Student',
+      'Score' => 'Score',
+      'Add Score' => 'Add Score',
+
+      //update
+      'Updating in progress...' => ' Updating in progress...',
+      'Select user' => 'Select user',
+      'Update email address' => 'Update email address',
+      'To change current user type' => 'To change current user type',
    ];
